56a749681057d7fa234b802503ff2c8254305390
earthicko <70836889+earthicko@users.noreply.github.com>
2021-07-30 17:57:12 +0900

    Input JSON

--- app/src/main/java/com/example/deliveryarcadeagent/MainActivity.java
+++ app/src/main/java/com/example/deliveryarcadeagent/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.deliveryarcadeagent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,20 +12,47 @@ import android.webkit.WebViewClient;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Objects;
 
 import io.socket.client.IO;
-import io.socket.client.Manager;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    public JSONObject readJSON(String fileName) {
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open(fileName);
+            int fileSize = inputStream.available();
 
-    Socket mSocket = IO.socket(URI.create("http://192.168.0.3:5010/app"));
+            byte[] buffer = new byte[fileSize];
+            inputStream.read(buffer);
+            inputStream.close();
+
+            String jsonData = new String(buffer, StandardCharsets.UTF_8);
+            JSONObject jsonObject = new JSONObject(jsonData);
+
+            return jsonObject;
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private JSONObject networkConfig;
+
+    private Socket socket;
 
     private final Emitter.Listener receiveInfo = new Emitter.Listener() {
         @Override
@@ -33,16 +61,34 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mSocket.on("info", receiveInfo);
-        mSocket.connect();
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        networkConfig = readJSON("config/network_config.json");
+        Log.i("config", String.valueOf(networkConfig));
+
+        String server_ip = "";
+        String server_port = "";
+        try {
+            server_ip = networkConfig.getJSONObject("server").getString("ip");
+            server_port = networkConfig.getJSONObject("server").getJSONObject("port").getString("control");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String socketIOURI = "http://" + server_ip + ":" + server_port + "/app";
+        Log.i("config", "Connecting to: " + socketIOURI);
+        socket = IO.socket(URI.create(socketIOURI));
+        socket.on("info", receiveInfo);
+        socket.connect();
+
         setContentView(R.layout.activity_main);
 
         WebView webcamView = findViewById(R.id.webcamView);
-        webcamView.loadUrl("http://192.168.0.3:5010/video");
+        String WebcamPageURI = "http://" + server_ip + ":" + server_port + "/video";
+        Log.i("config", "Connecting to: " + WebcamPageURI);
+        webcamView.loadUrl(WebcamPageURI);
         webcamView.setWebViewClient(new WebViewClient());
         WebSettings webcamViewSettings = webcamView.getSettings();
         webcamViewSettings.setJavaScriptEnabled(true);
@@ -61,70 +107,71 @@ public class MainActivity extends AppCompatActivity {
         throttleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mSocket.emit("command", "throttle " + progress);
+                socket.emit("command", "throttle " + progress);
                 throttleValueText.setText(String.valueOf(progress));
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                mSocket.emit("command", "throttle start");
+                socket.emit("command", "throttle start");
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                mSocket.emit("command", "throttle end");
+                socket.emit("command", "throttle end");
             }
         });
 
         steerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mSocket.emit("command", "steering " + progress);
+                socket.emit("command", "steering " + progress);
                 steerValueText.setText(String.valueOf(progress));
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                mSocket.emit("command", "steering start");
+                socket.emit("command", "steering start");
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                mSocket.emit("command", "steering end");
+                socket.emit("command", "steering end");
             }
         });
+
     }
 
     public void press_up(View view) {
         Log.i("button", "up");
-        mSocket.emit("command", "UP");
+        socket.emit("command", "UP");
     }
 
     public void press_down(View view) {
         Log.i("button", "down");
-        mSocket.emit("command", "DOWN");
+        socket.emit("command", "DOWN");
     }
 
     public void press_left(View view) {
         Log.i("button", "left");
-        mSocket.emit("command", "LEFT");
+        socket.emit("command", "LEFT");
     }
 
     public void press_right(View view) {
         Log.i("button", "right");
-        mSocket.emit("command", "RIGHT");
+        socket.emit("command", "RIGHT");
     }
 
     public void press_fire(View view) {
         Log.i("button", "fire");
-        mSocket.emit("command", "FIRE");
+        socket.emit("command", "FIRE");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
 
-        mSocket.disconnect();
-        mSocket.off("info", receiveInfo);
+        socket.disconnect();
+        socket.off("info", receiveInfo);
     }
 }
