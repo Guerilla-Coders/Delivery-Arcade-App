13ad823f3dc3341476db6250611caa76bc3c2ffc
earthicko <70836889+earthicko@users.noreply.github.com>
2021-08-08 12:02:59 +0900

    조이스틱 구현

--- app/src/main/java/com/example/deliveryarcadeagent/MainActivity.java
+++ app/src/main/java/com/example/deliveryarcadeagent/MainActivity.java
@@ -2,26 +2,29 @@ package com.example.deliveryarcadeagent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
+import io.github.controlwear.virtual.joystick.android.JoystickView;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
@@ -29,28 +32,61 @@ import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
 
-    public JSONObject readJSON(String fileName) {
-        AssetManager assetManager = getAssets();
-        try {
-            InputStream inputStream = assetManager.open(fileName);
-            int fileSize = inputStream.available();
+    private JSONObject networkConfig;
 
-            byte[] buffer = new byte[fileSize];
-            inputStream.read(buffer);
-            inputStream.close();
 
-            String jsonData = new String(buffer, StandardCharsets.UTF_8);
-            JSONObject jsonObject = new JSONObject(jsonData);
+    private static class JsonTask extends AsyncTask<String, String, JSONObject> {
 
-            return jsonObject;
+        protected JSONObject doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
 
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
+            try {
+                Log.i("config", "Connecting to " + params[0]);
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestProperty("User-Agent", "da-app");
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder buffer = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                    Log.i("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+                String jsonData = buffer.toString();
+
+                return new JSONObject(jsonData);
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
         }
-        return null;
+
+//        @Override
+//        protected void onPostExecute(JSONObject result) {
+//            super.onPostExecute(result);
+//            networkConfig = result;
+//        }
     }
 
-    private JSONObject networkConfig;
 
     private Socket socket;
 
@@ -66,7 +102,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        networkConfig = readJSON("config/network_config.json");
+        // networkConfig = readJSON("config/network_config.json");
+        try {
+            networkConfig = new JsonTask().execute("http://49.50.175.88:6000/config/server").get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Log.i("config", String.valueOf(networkConfig));
 
         String server_ip = "";
@@ -97,49 +139,22 @@ public class MainActivity extends AppCompatActivity {
         webcamViewSettings.setUseWideViewPort(true);
         webcamViewSettings.setSupportZoom(true);
 
+        JoystickView joystick_left = findViewById(R.id.joystickView_left);
+        JoystickView joystick_right = findViewById(R.id.joystickView_right);
 
-        SeekBar throttleSeekBar = (SeekBar) findViewById(R.id.throttleBar);
-        SeekBar steerSeekBar = (SeekBar) findViewById(R.id.steerBar);
-
-        TextView throttleValueText = (TextView) findViewById(R.id.throttleBarValueDisplay);
-        TextView steerValueText = (TextView) findViewById(R.id.steerBarValueDisplay);
-
-        throttleSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        joystick_left.setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                socket.emit("command", "throttle " + progress);
-                throttleValueText.setText(String.valueOf(progress));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                socket.emit("command", "throttle start");
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                socket.emit("command", "throttle end");
+            public void onMove(int angle, int strength) {
+                Log.d("joystick", "Left A " + angle + " S " + strength);
             }
         });
 
-        steerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        joystick_right.setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                socket.emit("command", "steering " + progress);
-                steerValueText.setText(String.valueOf(progress));
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                socket.emit("command", "steering start");
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                socket.emit("command", "steering end");
+            public void onMove(int angle, int strength) {
+                Log.d("joystick", "Right A " + angle + " S " + strength);
             }
         });
-
     }
 
     public void press_up(View view) {
@@ -160,11 +175,6 @@ public class MainActivity extends AppCompatActivity {
     public void press_right(View view) {
         Log.i("button", "right");
         socket.emit("command", "RIGHT");
-    }
-
-    public void press_fire(View view) {
-        Log.i("button", "fire");
-        socket.emit("command", "FIRE");
     }
 
     @Override
